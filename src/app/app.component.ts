673ef05c8b96888aff92f98224917c8419218b7d
Smart97 <smart9713@gmail.com>
2019-02-12 03:39:03 +0300

    Закончил тестовое задание, теперь теперь сабмиты неактивны до ввода данных.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  public hide = 'hide';
-  public auth = 'auth';
-  public account = 'account';
-}
+    public auth = 'auth';
+    public account = 'account';
+    public isShow1 = 'true';
+    public isShow2 = 'true';
+    }
 
-
+//     ngOnInit() {
+//         this.nameControl = new FormControl('', [Validators.required, Validators.minLength(3)]);
+//         this.nameControl.valueChanges.subscribe((value) => console.log(value));
+//         this.nameControl.statusChanges.subscribe((status) => console.log(status));
+//     }
+// }
